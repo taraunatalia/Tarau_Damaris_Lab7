7b4f55c9782b762aadc9fdcf3e0ecea1ac845368
Damaris Tarau <taraudamaris@yahoo.com>
2022-12-06 16:08:23 +0200

    text

--- Data/ShoppingListDatabase.cs
+++ Data/ShoppingListDatabase.cs
@@ -1,5 +1,6 @@
 ﻿using SQLite;
 using System;
+using System.Collections;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -16,31 +17,63 @@ namespace Tarau_Damaris_Lab7.Data
         {
             _database = new SQLiteAsyncConnection(dbPath);
             _database.CreateTableAsync<ShopList>().Wait();
+            _database.CreateTableAsync<Product>().Wait();
+            _database.CreateTableAsync<ListProduct>().Wait();
         }
-        public Task<List<ShopList>> GetShopListsAsync()
+        public Task<int> SaveProductAsync(Product product)
         {
-            return _database.Table<ShopList>().ToListAsync();
-        }
-        public Task<ShopList> GetShopListAsync(int id)
-        {
-            return _database.Table<ShopList>()
-            .Where(i => i.ID == id)
-           .FirstOrDefaultAsync();
-        }
-        public Task<int> SaveShopListAsync(ShopList slist)
-        {
-            if (slist.ID != 0)
+            if (product.ID != 0)
             {
-                return _database.UpdateAsync(slist);
+                return _database.UpdateAsync(product);
             }
             else
             {
-                return _database.InsertAsync(slist);
+                return _database.InsertAsync(product);
             }
         }
-        public Task<int> DeleteShopListAsync(ShopList slist)
+        public Task<int> DeleteProductAsync(Product product)
         {
-            return _database.DeleteAsync(slist);
+            return _database.DeleteAsync(product);
         }
+        public Task<List<Product>> GetProductsAsync()
+        {
+            return _database.Table<Product>().ToListAsync();
+        }
+
+        internal Task DeleteShopListAsync(ShopList slist)
+        {
+            throw new NotImplementedException();
+        }
+
+        internal Task SaveShopListAsync(ShopList slist)
+        {
+            throw new NotImplementedException();
+        }
+
+        internal Task<IEnumerable> GetShopListsAsync()
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task<int> SaveListProductAsync(ListProduct listp)
+        {
+            if (listp.ID != 0)
+            {
+                return _database.UpdateAsync(listp);
+            }
+            else
+            {
+                return _database.InsertAsync(listp);
+            }
+        }
+        public Task<List<Product>> GetListProductsAsync(int shoplistid)
+        {
+            return _database.QueryAsync<Product>(
+            "select P.ID, P.Description from Product P"
+            + " inner join ListProduct LP"
+            + " on P.ID = LP.ProductID where LP.ShopListID = ?",
+            shoplistid);
+        }
+
     }
 }
