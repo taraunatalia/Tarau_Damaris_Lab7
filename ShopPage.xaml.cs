e46287bae7c4d00acc21005c316360310e8ca28d
Damaris Tarau <taraudamaris@yahoo.com>
2023-01-02 15:57:21 +0200

    lab10/2

--- ShopPage.xaml.cs
+++ ShopPage.xaml.cs
@@ -25,6 +25,23 @@ public partial class ShopPage : ContentPage
        var location = locations?.FirstOrDefault();
         // var myLocation = await Geolocation.GetLocationAsync();
         var myLocation = new Location(46.7731796289, 23.6213886738);
+
+       /* var distance = myLocation.CalculateDistance(location,
+DistanceUnits.Kilometers);
+        if (distance < 4)
+        {
+            var request = new NotificationRequest
+            {
+                Title = "Ai de facut cumparaturi in apropiere!",
+                Description = address,
+                Schedule = new NotificationRequestSchedule
+                {
+                    NotifyTime = DateTime.Now.AddSeconds(1)
+                }
+            };
+            LocalNotificationCenter.Current.Show(request);
+        }*/
+
         await Map.OpenAsync(location, options);
     }
 
